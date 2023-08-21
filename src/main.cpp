6f9b3df2aea5568147369c910c5e8300c7860e92
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-08-21 17:12:22 +0200

    Add dockspace
    We don't see the rendering yet as we don't have framebuffer yet.

--- src/main.cpp
+++ src/main.cpp
@@ -134,34 +134,99 @@ public:
 
     virtual void OnImGuiRender() override {
         VXM_PROFILE_FUNCTION();
-//        ImGui::Begin("Camera Component");
-//
-//        glm::vec3 position = m_Camera.GetPosition();
-//        ImGui::DragFloat3("Camera Position", glm::value_ptr(position));
-//        if(position != m_Camera.GetPosition()) {
-//            m_Camera.SetPosition(position);
-//        }
-//
-//        glm::quat rotation = m_Camera.GetRotation();
-//        glm::vec3 euler = glm::degrees(glm::eulerAngles(rotation));
-//        ImGui::DragFloat3("Camera Rotation", glm::value_ptr(euler));
-//        if(euler != glm::degrees(glm::eulerAngles(m_Camera.GetRotation()))) {
-//            m_Camera.SetRotation(glm::quat(glm::radians(euler)));
-//        }
-//
-//        float fov = m_Camera.GetFOV();
-//        ImGui::SliderFloat("fov", &fov, 45.0f, 110.0f);
-//        if(fov != m_Camera.GetFOV()){
-//            m_Camera.SetFOV(fov);
-//        }
-//
-//        ImGui::SliderFloat("sensitivity", &sensitivity, 0.01f, 2.0f);
-//        ImGui::DragFloat("speed", &speed, 0.01f);
-//
-//        bool vsync = Voxymore::Core::Application::Get().GetWindow().IsVSync();
-//        ImGui::Checkbox("VSync", &vsync);
-//        Voxymore::Core::Application::Get().GetWindow().SetVSync(vsync);
-//        ImGui::End();
+        {
+            VXM_PROFILE_SCOPE("ExampleLayer::OnImGuiRender -> Dockspace");
+            // READ THIS !!!
+            // TL;DR; this demo is more complicated than what most users you would normally use.
+            // If we remove all options we are showcasing, this demo would become:
+            //     void ShowExampleAppDockSpace()
+            //     {
+            //         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+            //     }
+            // In most cases you should be able to just call DockSpaceOverViewport() and ignore all the code below!
+            // In this specific demo, we are not using DockSpaceOverViewport() because:
+            // - (1) we allow the host window to be floating/moveable instead of filling the viewport (when opt_fullscreen == false)
+            // - (2) we allow the host window to have padding (when opt_padding == true)
+            // - (3) we expose many flags and need a way to have them visible.
+            // - (4) we have a local menu bar in the host window (vs. you could use BeginMainMenuBar() + DockSpaceOverViewport()
+            //      in your code, but we don't here because we allow the window to be floating)
+
+            static bool dockspaceOpen = true;
+            static bool opt_fullscreen = true;
+            static bool opt_padding = false;
+            static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+            // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+            // because it would be confusing to have two docking targets within each others.
+            ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+            if (opt_fullscreen)
+            {
+                const ImGuiViewport* viewport = ImGui::GetMainViewport();
+                ImGui::SetNextWindowPos(viewport->WorkPos);
+                ImGui::SetNextWindowSize(viewport->WorkSize);
+                ImGui::SetNextWindowViewport(viewport->ID);
+                ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+                ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+                window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+                window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+            }
+            else
+            {
+                dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+            }
+
+            // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+            // and handle the pass-thru hole, so we ask Begin() to not render a background.
+            if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+                window_flags |= ImGuiWindowFlags_NoBackground;
+
+            // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+            // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+            // all active windows docked into it will lose their parent and become undocked.
+            // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+            // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+            if (!opt_padding)
+                ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+            ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
+            if (!opt_padding)
+                ImGui::PopStyleVar();
+
+            if (opt_fullscreen)
+                ImGui::PopStyleVar(2);
+
+            // Submit the DockSpace
+            ImGuiIO& io = ImGui::GetIO();
+            if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+            {
+                ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+                ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+            }
+            else
+            {
+                ImGuiIO& io = ImGui::GetIO();
+                ImGui::Text("ERROR: Docking is not enabled! See Demo > Configuration.");
+                ImGui::Text("Set io.ConfigFlags |= ImGuiConfigFlags_DockingEnable in your code, or ");
+                ImGui::SameLine(0.0f, 0.0f);
+                if (ImGui::SmallButton("click here"))
+                    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+            }
+
+            if (ImGui::BeginMenuBar())
+            {
+                if (ImGui::BeginMenu("Options"))
+                {
+                    if(ImGui::MenuItem("Fullscreen", ""))
+                    {
+                        Voxymore::Core::Application::Get().Close();
+                    }
+                    ImGui::EndMenu();
+                }
+
+                ImGui::EndMenuBar();
+            }
+
+            ImGui::End();
+        }
 
         {
             VXM_PROFILE_SCOPE("ExampleLayer::OnImGuiRender -> Model Component drawing");
@@ -173,6 +238,7 @@ public:
 
             ImGui::End();
         }
+
     }
 
     virtual void OnEvent(Voxymore::Core::Event& event) override {
